643ce6abc5550c3162429d056e0ba1adc6ea3c0f
avid-modal-4107 <norbertl@buffalo.edu>
2018-12-15 10:46:36 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+def dictionary(word)
+  dict = {
+    "hello" => "hi",
+    "to" => "2",
+    "too" => "2",
+    "two" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+  if word in dict
+    return dict[word]
+end
+
+def word_substituter(string)
+  output = []
+  string.split.each do |s|
+    
